621c928dacd21b8140544c3ce31b5d2d36bdd5ba
Ayoub ELHARRAN <elharran.ayoub@inemail.ine.inpt.ma>
2024-05-09 17:16:47 +0100

    functions added

--- caption_scraper.py
+++ caption_scraper.py
@@ -1,38 +1,23 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 import time
+from functions import *
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=options)  
-# driver.get("https://www.youtube.com/watch?v=nssOuD9EcVk&ab_channel=RunThat")
-driver.get("https://www.youtube.com/watch?v=YDfiTGGPYCk&ab_channel=LiveNOWfromFOX")
+# Getting the chrome driver
+driver = getDriver()
+
+# Going to the link
+# link = "https://www.youtube.com/watch?v=gCNeDWCI0vo&ab_channel=AlJazeeraEnglish"
+link = "https://www.youtube.com/watch?v=YDfiTGGPYCk&ab_channel=LiveNOWfromFOX"
+goToLink(link, driver)
+
+playButton()
+
+# Finding the captions container
+caption_container = getCaptionsContainer(driver)
+print("Caption container Found!!")
 
 
-# caption_button = driver.find_element(By.CLASS_NAME, "ytp-subtitles-button")
-# # print(caption_button)
-# caption_button.click()
-# print("caption button clicked")
-
-# play_button = driver.find_element(By.CLASS_NAME, "ytp-large-play-button")
-
-# This one is to handle the play button that can appear in case the video didn't start playing automatically
-try:
-    play_button = WebDriverWait(driver, 3).until(
-        EC.element_to_be_clickable((By.CLASS_NAME, "ytp-large-play-button"))
-    )
-    play_button.click()
-    print("play button clicked") 
-except TimeoutException:
-    print("The video started playing automatically.")
-
-# time.sleep(3) # to skip this caption at first: "English (auto-generated) click  for settings"
-
-caption_container = driver.find_element(By.CLASS_NAME, "ytp-caption-window-container")
-# print(caption_container)
+last_text="."
 while True:
 
     try:
@@ -40,6 +25,7 @@ while True:
         caption_lines = caption_container.find_elements(By.CLASS_NAME, "caption-visual-line")
         if len(caption_lines):
             for line in caption_lines:
+                # print(line)
                 # Extract text from each segment within a line
                 text = line.text
                 if text != last_text:
@@ -48,4 +34,3 @@ while True:
         time.sleep(1.5)
     except:
         print('No caption found')
-        # caption_container = driver.find_element(By.CLASS_NAME, "ytp-caption-window-container")
---
+++ functions.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+def getDriver() -> webdriver.Chrome:
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(options=options) 
+    return driver
+
+def goToLink(link:str, driver: webdriver.Chrome) -> None:
+    driver.get(link)
+
+def playButton(driver: webdriver.Chrome) -> None:
+    """
+    This one is to handle the play button that can appear in case the video didn't start playing automatically
+    """
+    try:
+        play_button = WebDriverWait(driver, 3).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "ytp-large-play-button"))
+        )
+        play_button.click()
+        print("play button clicked") 
+    except TimeoutException:
+        print("The video started playing automatically.")
+
+def getCaptionsContainer(driver: webdriver.Chrome) -> WebElement:
+    caption_container = driver.find_element(By.CLASS_NAME, "ytp-caption-window-container")
+    return caption_container
+
+def getCaptionsLines(caption_container: WebElement) -> WebElement :
+    """
+    Extract all caption lines
+    """
+    caption_lines = caption_container.find_elements(By.CLASS_NAME, "caption-visual-line")
+    if len(caption_lines):
+        # Extract text from each a line
+        for line in caption_lines:
+            text = line.text
+            if text != last_text:
+                print(text)
+                last_text=text
+    return text
